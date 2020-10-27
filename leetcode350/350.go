3ec1cf43d61519b349d9f5d0814adbc1dcfea6e1
kucfire <cai972316027@qq.com>
2020-10-27 10:34:31 +0800

    update leetcode350

---
+++ leetcode350/350.go
@@ -0,0 +1,22 @@
+package leetcode350
+
+import "sort"
+
+// Intersect : method of sort
+func Intersect(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	result := make([]int, 0)
+	start := 0
+	for _, num := range nums1 {
+	loop2:
+		for i := start; i < len(nums2); i++ {
+			if num == nums2[i] {
+				result = append(result, num)
+				start = i + 1
+				break loop2
+			}
+		}
+	}
+	return result
+}
