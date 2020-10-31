ecc979908479944151c512767ddc1b626da4174c
kucfire <cai972316027@qq.com>
2020-10-31 20:27:00 +0800

    update leetcode387

--- leetcode387/387_test.go
+++ leetcode387/387_test.go
@@ -18,3 +18,20 @@ func TestFirstUniqChar(t *testing.T) {
 		}
 	}
 }
+
+func TestFirstUniqChar2(t *testing.T) {
+	datas := []struct {
+		s      string
+		result int
+	}{
+		{"leetcode", 0},
+		{"loveleetcode", 2},
+		{"aadadaad", -1},
+	}
+
+	for _, data := range datas {
+		if actual := FirstUniqChar2(data.s); actual != data.result {
+			t.Errorf("actually got %d, but expect %d", actual, data.result)
+		}
+	}
+}
