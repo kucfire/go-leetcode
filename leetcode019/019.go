80d043e8501175496e5f8e7054d2601b253286d3
kucfire <cai972316027@qq.com>
2020-11-03 10:56:15 +0800

    update leetcode019

---
+++ leetcode019/019.go
@@ -0,0 +1,27 @@
+/*
+	leetcode tag:019 title:删除链表的倒数第N个结点
+*/
+
+package leetcode019
+
+// ListNode : body
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// normal method
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	first := head
+	second := &ListNode{0, head}
+	for n > 0 {
+		first = first.Next
+		n--
+	}
+	result := second
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return result.Next
+}
