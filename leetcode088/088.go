f36a4538f12eb2f049bdebd020161a94f35ca937
kucfire <cai972316027@qq.com>
2020-11-07 16:35:12 +0800

    update leetcode088

---
+++ leetcode088/088.go
@@ -0,0 +1,15 @@
+/*
+	leetcode tag:088 title:合并两个有序数组
+*/
+
+package leetcode088
+
+import "sort"
+
+// normal method
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for i, j := m, 0; i < m+n && j < n; i, j = i+1, j+1 {
+		nums1[i] = nums2[j]
+	}
+	sort.Ints(nums1)
+}
