134783ac15e7d764120489942dea465dc4735677
kucfire <cai972316027@qq.com>
2020-11-08 11:56:36 +0800

    update leetcode121

---
+++ leetcode121/121.go
@@ -0,0 +1,47 @@
+/*
+	leetcode tag:121 title:买卖股票的最佳时机
+*/
+
+package leetcode121
+
+// MaxProfit : one time to get the most money(method of double loop)
+func MaxProfit(prices []int) int {
+	// 排除异常传入数值
+	if len(prices) < 2 {
+		return 0
+	}
+	var result int
+	for i := 0; i < len(prices)-1; i++ {
+		for j := i + 1; j < len(prices); j++ {
+			tmp := prices[j] - prices[i]
+			if tmp > 0 && tmp > result {
+				result = tmp
+			} else {
+
+			}
+		}
+	}
+	return result
+}
+
+// MaxProfit2 :
+func MaxProfit2(prices []int) int {
+	// 排除异常传入数值
+	if len(prices) < 2 {
+		return 0
+	}
+	var result int
+	for head, tail := 0, 1; head < tail && head < len(prices)-2 && tail < len(prices)-1; {
+		tmp := prices[tail] - prices[head]
+		if tmp < 0 { // 代表有耕地的买入价，head可以看作是寻找len(prices)-1中最低的买入价格，tail可以看做是寻找1~len(prices)中最高的卖出价格，result记录最大的利润值
+			head = tail
+			tail++
+			continue
+		}
+		if tmp > result {
+			result = tmp
+		}
+		tail++
+	}
+	return result
+}
---
+++ leetcode121/121_test.go
@@ -0,0 +1,35 @@
+package leetcode121
+
+import "testing"
+
+func TestMaxProfit(t *testing.T) {
+	datas := []struct {
+		prices []int
+		result int
+	}{
+		{[]int{7, 1, 5, 3, 6, 4}, 5},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for i, data := range datas {
+		if actuall := MaxProfit(data.prices); actuall != data.result {
+			t.Errorf("example %d actually got %d, but expect %d", i+1, actuall, data.result)
+		}
+	}
+}
+
+func TestMaxProfit2(t *testing.T) {
+	datas := []struct {
+		prices []int
+		result int
+	}{
+		{[]int{7, 1, 5, 3, 6, 4}, 5},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+
+	for i, data := range datas {
+		if actuall := MaxProfit2(data.prices); actuall != data.result {
+			t.Errorf("example %d actually got %d, but expect %d", i+1, actuall, data.result)
+		}
+	}
+}
