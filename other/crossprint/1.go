1516fa9cbca85ccad83dc59882eace40c2ed97c7
kucfire <cai972316027@qq.com>
2020-12-02 14:14:41 +0800

    update other

--- other/crossprint/1.go
+++ other/crossprint/1.go
@@ -9,20 +9,20 @@ var pool = 100
 
 // Goroutine1 : 模拟生产者,打印奇数
 func Goroutine1(p chan int) {
-	for i := 0; i < pool; i++ {
+	for i := 1; i <= pool; i++ {
 		p <- i
 		if i%2 == 1 {
-			fmt.Println(i)
+			fmt.Printf("Goroutine1 : %d\n", i)
 		}
 	}
 }
 
 // Goroutine2 ： 模拟消费者，打印偶数
 func Goroutine2(p chan int) {
-	for i := 0; i < pool; i++ {
+	for i := 1; i <= pool; i++ {
 		<-p
 		if i%2 == 0 {
-			fmt.Println(i)
+			fmt.Printf("Goroutine2 : %d\n", i)
 		}
 	}
 }
