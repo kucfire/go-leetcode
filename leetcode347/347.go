73cdc5d65efce86aff99997441ad82595390e297
kucfire <cai972316027@qq.com>
2020-12-15 00:49:14 +0800

    update leetcode347

--- leetcode347/347.go
+++ leetcode347/347.go
@@ -1,8 +1,6 @@
 package leetcode347
 
 func TopKFrequent(nums []int, k int) []int {
-	// if len(nums) < 2 || k
-
 	// 计数
 	arrayMap := make(map[int]int)
 	arrayList := make([]int, 0)
@@ -22,13 +20,31 @@ func TopKFrequent(nums []int, k int) []int {
 	// })
 
 	// 快速排序myself
-	mv := func(datas []int, start, move int) int {
+	var quicksort func(arrayMap map[int]int, datas []int, left, right int)
+	quicksort = func(arrayMap map[int]int, datas []int, left, right int) {
+		// 退出条件
+		if left >= right {
+			return
+		}
 
+		// 取最后一位当中间值
+		middleNum := arrayMap[datas[right]]
+		// start从最左侧的外边开始（left-1）
+		start := left - 1
+		// 从left开始遍历，遇到比middleNum小的值，start+1，然后datas[j]跟datas[start]交换位置
+		for j := left; j < right; j++ {
+			if arrayMap[datas[j]] > middleNum {
+				start++
+				datas[j], datas[start] = datas[start], datas[j]
+			}
+		}
+		// 遍历完成后，先将start右侧的值跟middleNum的值交换，从而，start+1左侧的值比start+1小，start+1右侧的值比start的大，形成两个区域
+		datas[start+1], datas[right] = datas[right], datas[start+1]
+		// 两个区域继续执行，直到达到退出条件
+		quicksort(arrayMap, datas, left, start)
+		quicksort(arrayMap, datas, start+2, right)
 	}
-	quicksort := func(datas []int, left, right int) {
-
-	}
-	quicksort(arrayList, 0, len(arrayList)-1)
+	quicksort(arrayMap, arrayList, 0, len(arrayList)-1)
 
 	if len(nums) < 2 || k > len(nums) {
 		return arrayList
--- leetcode347/347_test.go
+++ leetcode347/347_test.go
@@ -13,11 +13,12 @@ func TestTopKFrequent(t *testing.T) {
 	}{
 		{[]int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
 		{[]int{1}, 1, []int{1}},
+		{[]int{4, 1, -1, 2, -1, 2, 3}, 2, []int{2, -1}},
 	}
 
 	for i, data := range datas {
 		if actual := TopKFrequent(data.nums, data.k); !reflect.DeepEqual(actual, data.result) {
-			t.Errorf("example %d actually got %v, but expect %v", i, actual, data.result)
+			t.Errorf("example %d actually got %v, but expect %v", i+1, actual, data.result)
 		}
 	}
 }
