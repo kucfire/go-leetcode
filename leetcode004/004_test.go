8ce31e40088f4ed1b9b18fd219e5cbe7e65c3267
kucfire <cai972316027@qq.com>
2020-12-30 19:44:49 +0800

    update leetcode004

---
+++ leetcode004/004_test.go
@@ -0,0 +1,22 @@
+package leetcode004
+
+import "testing"
+
+func TestFindMedianSortedArrays(t *testing.T) {
+	datas := []struct {
+		nums1, nums2 []int
+		result       float64
+	}{
+		{[]int{1, 3}, []int{2}, 2.00000},
+		{[]int{1, 2}, []int{3, 4}, 2.50000},
+		{[]int{0, 0}, []int{0, 0}, 0.00000},
+		{[]int{}, []int{1}, 1.00000},
+		{[]int{2}, []int{}, 2.00000},
+	}
+
+	for i, data := range datas {
+		if actual := FindMedianSortedArrays(data.nums1, data.nums2); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
