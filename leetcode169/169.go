e4a028ad458979a7dfe126d9b6b6204a275504ad
kucfire <cai972316027@qq.com>
2021-01-26 11:10:19 +0800

    update leetcode169

--- leetcode169/169.go
+++ leetcode169/169.go
@@ -1,1 +1,71 @@
+/*
+	leetcode tag:169 title:多数元素
+	point:
+	1.出现次数 大于 ⌊ n/2 ⌋ 的元素
+	2.时间复杂度为 O(n)、空间复杂度为 O(1)
+*/
+
 package leetcode169
+
+import (
+	"sort"
+)
+
+// method of count
+func MajorityElementCount(nums []int) int {
+	count, major := 0, 0
+	for _, num := range nums {
+		if count == 0 {
+			major = num
+		}
+		if num == major {
+			count++
+		} else {
+			count--
+		}
+	}
+	return major
+}
+
+// method of hashmap
+func MajorityElementHashmap(nums []int) int {
+	arrayMap := make(map[int]int)
+	for _, num := range nums {
+		arrayMap[num]++
+	}
+	for key, value := range arrayMap {
+		if value > len(nums)/2 {
+			return key
+		}
+	}
+	return -1
+}
+
+// 排序
+func MajorityElementSort(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)/2]
+}
+
+// 位运算(不能用)
+func MajorityElementBit(nums []int) int {
+	result := 0
+	signals := 0
+	for i := 0; i < 32; i++ {
+		ones := 0
+		for _, num := range nums {
+			if num < 0 {
+				signals = 1
+			}
+			ones += (num >> i) & 1
+			if ones > len(nums)/2 {
+				result += 1 << i
+				break
+			}
+		}
+	}
+	if result > 0 && signals == 1 {
+		result = -result
+	}
+	return result
+}
---
+++ leetcode169/169_test.go
@@ -0,0 +1,76 @@
+package leetcode169
+
+import "testing"
+
+func TestMajorityElementCount(t *testing.T) {
+	datas := []struct {
+		nums   []int
+		result int
+	}{
+		{[]int{3, 2, 3}, 3},
+		{[]int{2, 2, 1, 1, 1, 2, 2}, 2},
+		{[]int{1}, 1},
+		{[]int{2, 2}, 2},
+	}
+
+	for i, data := range datas {
+		if actual := MajorityElementCount(data.nums); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
+
+func TestMajorityElementHashmap(t *testing.T) {
+	datas := []struct {
+		nums   []int
+		result int
+	}{
+		{[]int{3, 2, 3}, 3},
+		{[]int{2, 2, 1, 1, 1, 2, 2}, 2},
+		{[]int{1}, 1},
+		{[]int{2, 2}, 2},
+	}
+
+	for i, data := range datas {
+		if actual := MajorityElementHashmap(data.nums); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
+
+func TestMajorityElementSort(t *testing.T) {
+	datas := []struct {
+		nums   []int
+		result int
+	}{
+		{[]int{3, 2, 3}, 3},
+		{[]int{2, 2, 1, 1, 1, 2, 2}, 2},
+		{[]int{1}, 1},
+		{[]int{2, 2}, 2},
+	}
+
+	for i, data := range datas {
+		if actual := MajorityElementSort(data.nums); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
+
+func TestMajorityElementBit(t *testing.T) {
+	datas := []struct {
+		nums   []int
+		result int
+	}{
+		{[]int{3, 2, 3}, 3},
+		{[]int{2, 2, 1, 1, 1, 2, 2}, 2},
+		{[]int{1}, 1},
+		{[]int{2, 2}, 2},
+		{[]int{-2147483648}, -2147483648},
+	}
+
+	for i, data := range datas {
+		if actual := MajorityElementBit(data.nums); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
