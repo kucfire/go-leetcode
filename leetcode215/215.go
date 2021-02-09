cefdfa088c03f930e62a7ab8c9738e6499cbd1e7
kucfire <cai972316027@qq.com>
2021-02-09 17:06:23 +0800

    update leetcode215

--- leetcode215/215.go
+++ leetcode215/215.go
@@ -1,5 +1,31 @@
+/*
+	leetcode tag:215 title:数组中第k大元素
+*/
+
 package leetcode215
 
-func FindKthLargest(nums []int, k int) int {
+import "sort"
 
+func FindKthLargest(nums []int, k int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-k]
+}
+
+// 自己写排序(性能极差)
+func FindKthLargestMyself(nums []int, k int) int {
+	var Sorts func(nums []int)
+	Sorts = func(nums []int) {
+		if len(nums)-1 == 0 {
+			return
+		}
+
+		for i := 1; i < len(nums); i++ {
+			if nums[i] <= nums[i-1] {
+				nums[i], nums[i-1] = nums[i-1], nums[i]
+			}
+		}
+		Sorts(nums[:len(nums)-1])
+	}
+	Sorts(nums)
+	return nums[len(nums)-k]
 }
--- leetcode215/215_test.go
+++ leetcode215/215_test.go
@@ -18,3 +18,20 @@ func TestFindKthLargest(t *testing.T) {
 		}
 	}
 }
+
+func TestFindKthLargestMyself(t *testing.T) {
+	datas := []struct {
+		nums   []int
+		k      int
+		result int
+	}{
+		{[]int{3, 2, 1, 5, 6, 4}, 2, 5},
+		{[]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4, 4},
+	}
+
+	for i, data := range datas {
+		if actual := FindKthLargestMyself(data.nums, data.k); actual != data.result {
+			t.Errorf("Example %d actually got %v, but expect %v", i+1, actual, data.result)
+		}
+	}
+}
