c84eb63dd29acc0979c1ecdfc1fc78b6f4698ca9
kucfire <cai972316027@qq.com>
2021-02-18 10:37:04 +0800

    update leetcode347

--- leetcode347/347_reverse.go
+++ leetcode347/347_reverse.go
@@ -26,7 +26,7 @@ func TopKFrequent_Reverse(nums []int, k int) []int {
 		start := left - 1
 		// 退出条件是到达middle所在位置的前一位，不能跟middle所在位置进行比较
 		for i := left; i < right; i++ {
-			if arrayMap[arrayList[i]] > middle {
+			if arrayMap[arrayList[i]] > middle { // 由于题干是要取前k个高频元素，所以数组需要从大到小排列
 				start++
 				arrayList[i], arrayList[start] = arrayList[start], arrayList[i]
 			}
@@ -40,5 +40,42 @@ func TopKFrequent_Reverse(nums []int, k int) []int {
 	}
 
 	quickSort(arrayList, arrayMap, 0, len(arrayList)-1)
+	return arrayList[:k]
+}
+
+func TopKFrequent_Reverse2(nums []int, k int) []int {
+	arrayMap := make(map[int]int)
+	arrayList := make([]int, 0)
+	for _, num := range nums {
+		_, ok := arrayMap[num]
+		if !ok {
+			arrayList = append(arrayList, num)
+		}
+		arrayMap[num]++
+	}
+
+	var fastSort func(arrayList []int, arrayMap map[int]int, left, right int)
+	fastSort = func(arrayList []int, arrayMap map[int]int, left, right int) {
+		if left >= right {
+			return
+		}
+
+		middle := arrayMap[arrayList[right]]
+		start := left - 1
+		for i := left; i < right; i++ {
+			if arrayMap[arrayList[i]] > middle {
+				start++
+				arrayList[i], arrayList[start] = arrayList[start], arrayList[i]
+			}
+		}
+
+		arrayList[start+1], arrayList[right] = arrayList[right], arrayList[start+1]
+
+		fastSort(arrayList, arrayMap, left, start)
+		fastSort(arrayList, arrayMap, start+2, right)
+	}
+
+	fastSort(arrayList, arrayMap, 0, len(arrayList)-1)
+
 	return arrayList[:k]
 }
